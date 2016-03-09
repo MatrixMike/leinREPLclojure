b18c16201520f6232ba7b06d4547160400da49e2
MatrixMike <matroxmike@icloud.com>
2016-03-09 12:29:19 +1100

    converted Calendar example
    runs from java -jar <jar.file>

---
+++ demo1/project.clj
@@ -0,0 +1,5 @@
+(defproject demo1 "1.0.1-SNAPSHOT"
+  :description "demo1 Calendar"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  
+  :main demo1.core)
---
+++ demo1/test/demo1/test/core.clj
@@ -0,0 +1,6 @@
+(ns demo1.test.core
+  (:use [demo1.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
