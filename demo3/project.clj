f9053ec8ce6d81abad4b8e258dad3cffa096bf85
MatrixMike <matroxmike@icloud.com>
2016-03-16 20:34:29 +1100

    demo3 added

---
+++ demo3/project.clj
@@ -0,0 +1,7 @@
+(defproject demo3 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  
+    :main demo3.core)
+    
+  
---
+++ demo3/src/demo3/core.clj
@@ -0,0 +1,21 @@
+(ns demo3.core
+  (:gen-class )
+)
+
+
+(import '(java.util Calendar GregorianCalendar))
+(let [gc (GregorianCalendar.)
+      day-of-week (.get gc Calendar/DAY_OF_WEEK)
+      is-weekend (or (= day-of-week Calendar/SATURDAY) (= day-of-week Calendar/SUNDAY))]
+  (if is-weekend
+    (println "play")
+    (do (println "work")
+        (println "sleep"))))
+        
+        
+(defn -main [& args]        
+   (when is-weekend (println "play"))
+(when-not is-weekend (println "\n") (println "work") (println "sleep"))  ;; force a newline even in REPL
+)
+
+     
