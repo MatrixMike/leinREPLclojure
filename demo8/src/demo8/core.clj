c0b7cef218622df261beae9249a0a5b6b7732331
MatrixMike <matroxmike@icloud.com>
2017-06-15 13:37:51 +1000

    initial commit

---
+++ demo8/src/demo8/core.clj
@@ -0,0 +1,1 @@
+(ns demo8.core)
---
+++ demo8/test/demo8/test/core.clj
@@ -0,0 +1,6 @@
+(ns demo8.test.core
+  (:use [demo8.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
