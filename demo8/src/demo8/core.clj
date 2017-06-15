16dcc8695b4c22d4f2f158e12c88f9a21eaf3d93
MatrixMike <matroxmike@icloud.com>
2017-06-15 13:45:53 +1000

    first working useful before adding body

--- demo8/src/demo8/core.clj
+++ demo8/src/demo8/core.clj
@@ -1,1 +1,15 @@
-(ns demo8.core)
+(ns demo8.core
+
+ (:gen-class)
+;;  (:import utils.demo)
+  (:import [javax.swing JPanel JFrame]
+           [javax.swing SwingUtilities]
+           [java.awt Dimension]
+           [java.awt Color]
+           [utils.demo])
+)  
+
+(defn -main [& args] 
+
+(println "hi there") ;; (System/exit 0))   
+)
